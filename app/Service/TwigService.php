6d714f5d2e7bb80d76ebf03669bbc3b5cd4e9576
Jascha030 <contact@jaschavanaalst.nl>
2021-10-12 22:36:00 +0200

    TwigService

---
+++ app/Service/TwigService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jascha030\TwigTheme\Service;
+
+use Twig\Environment;
+
+class TwigService implements TwigServiceInterface
+{
+    private Environment $environment;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(Environment $environment) {
+        $this->environment = $environment;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function getString(string $template, array $context): string
+    {
+        return $this->environment->render($template, $context);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function render(string $template, array $context): void
+    {
+        ob_start();
+        echo $this->getString($template, $context);
+
+        echo ob_get_clean();
+    }
+}

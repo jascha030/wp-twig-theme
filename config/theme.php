5130f422ac87d47d4872db6100907cad5da4bb11
Jascha030 <contact@jaschavanaalst.nl>
2021-10-13 23:50:13 +0200

    Add standard translation domain

--- config/theme.php
+++ config/theme.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Theme container definitions for php-di/php-di
- *
- * @see  ContainerInterface
- * @see  \DI\Container
- * @link https://php-di.org/doc/php-definitions.html
- */
 return [
-
+    'theme.domain' => 'WP_TT'
 ];
